6d84f834cc089f3b7bc37844b9b5db79847d0849
Pavithrapavvii <paviprajju94@gmail.com>
2023-07-12 05:28:05 -0700

    commit5

--- realestate/src/main/java/com/realestate/genericclass/ListenerImplimentation.java
+++ realestate/src/main/java/com/realestate/genericclass/ListenerImplimentation.java
@@ -13,17 +13,14 @@ import org.testng.ITestResult;
 
 public class ListenerImplimentation implements ITestListener{
 
-	@Override
 	public void onTestStart(ITestResult result) {
 		
 	}
 
-	@Override
 	public void onTestSuccess(ITestResult result) {
 		
 	}
 
-	@Override
 	public void onTestFailure(ITestResult result) {
 		String methodName = result.getMethod().getMethodName();
 		TakesScreenshot takescreenshot=(TakesScreenshot)BaseClass.sdriver;
@@ -39,27 +36,22 @@ public class ListenerImplimentation implements ITestListener{
 		
 	}
 
-	@Override
 	public void onTestSkipped(ITestResult result) {
 	
 	}
 
-	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		
 	}
 
-	@Override
 	public void onTestFailedWithTimeout(ITestResult result) {
 		
 	}
 
-	@Override
 	public void onStart(ITestContext context) {
 		
 	}
 
-	@Override
 	public void onFinish(ITestContext context) {
 		
 	}
